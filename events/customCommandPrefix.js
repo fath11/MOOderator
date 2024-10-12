54356a3e56e82c3cec0c35d6467b7b8f76976919
fath11 <fathimkamal@gmail.com>
2024-10-12 16:18:08 +0800

    you are loved 2

--- events/customCommandPrefix.js
+++ events/customCommandPrefix.js
@@ -12,9 +12,9 @@ module.exports = {
     switch(command) {
       case "ihatemyself":
         const duration = 60 * 1000; // 1 minute in milliseconds
+        message.reply("DO NOT SAY THAT. You deserves to be loved and cared, you are special, believe in yourself, we care about you. \n You should take some time off of Discord and take a rest, stay strong for me will ya 💪");
         try {
-          await message.member.timeout(duration, 'Taking a rest');
-          message.reply("DO NOT SAY THAT. You deserves to be loved and cared, you are special, believe in yourself, we care about you. \n You should take some time off of Discord and take a rest, stay strong for me will ya 💪");
+          await message.member.timeout(duration, 'Take a rest from this evil place...');
         } catch (error) {
           console.error('Failed to timeout the user:', error);
           message.reply('Failed to timeout the user.');
