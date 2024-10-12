652b644cf1e98ab579deac33bc1b5943f37ebc49
fath11 <fathimkamal@gmail.com>
2024-10-12 13:14:03 +0800

    uses env variables now

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -36,4 +35,4 @@ for (const file of eventFiles) {
 	}
 }
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
